7b1e9ebf42daaef589816d79bc729fcf05f01d59
Jin-seop <403gh@naver.com>
2026-04-07 22:28:05 +0900

    chore: HomeScreen 컴포넌트에서 주석 처리된 코드 제거
    - 사용하지 않는 API 호출 및 로딩 상태 처리 코드 주석 처리
    - 코드 가독성을 높이기 위해 불필요한 주석 제거

--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -1,23 +1,21 @@
 import { useQuery } from '@tanstack/react-query';
-import client from '../api/client';
+// import client from '../api/client';
 import React from 'react';
 import { Text, FlatList } from 'react-native';
 
 const HomeScreen = () => {
-  const { data, isLoading, error } = useQuery({
-    queryKey: ['recommendedUsers'],
-    queryFn: async () => {
-      const res = await client.get('/user/recommend');
-      return res.data;
-    },
-  });
-
-  if (isLoading) return <Text>로딩 중...</Text>;
-  if (error) return <Text>에러 발생!</Text>;
-
-  return (
-    <FlatList data={data} renderItem={({ item }) => <UserCard user={item} />} />
-  );
+  // const { data, isLoading, error } = useQuery({
+  //   queryKey: ['recommendedUsers'],
+  //   queryFn: async () => {
+  //     const res = await client.get('/user/recommend');
+  //     return res.data;
+  //   },
+  // });
+  // if (isLoading) return <Text>로딩 중...</Text>;
+  // if (error) return <Text>에러 발생!</Text>;
+  // return (
+  //   <FlatList data={data} renderItem={({ item }) => <UserCard user={item} />} />
+  // );
 };
 
 export default HomeScreen;
